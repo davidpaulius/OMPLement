607e7d45b250544d393e9ce09240458ca6fcdce7
davidpaulius <paulius.david@gmail.com>
2025-01-23 13:29:01 -0500

    Made some major updates to add state validation for motion planning

--- OMPLement.py
+++ OMPLement.py
@@ -50,7 +50,7 @@ function luaFindIKConfig(data)
         maxTime = 10,
         findMultiple = false, -- change to True to find multiple solutions
         pMetric = {0.05,0.05,0.05,0.1},
-        --cb = "stateValidationCollision_lua",
+        --cb = 'stateValidationCollision_lua'
     }
 
     local configs = simIK.findConfigs(ikEnv, ikGroup, ikJointHandles, params)
@@ -71,6 +71,10 @@ function luaOMPLCompute(data)
     local max_compute = data["max_compute"]
     local max_simplify = data["max_simplify"]
     local len_path = data["len_path"]
+
+    if data["use_state_validation"] and data["use_state_validation"] == true then
+        simOMPL.setStateValidationCallback(ompl_task, 'stateValidationCollision_lua')
+    end
 
     -- run the OMPL function from lua:
     result, path = simOMPL.compute(ompl_task, max_compute, max_simplify, len_path)
@@ -215,6 +219,49 @@ def stateValidationCollision_python(config):
 
     return not bool(is_collision)
 
+
+def stateValidationOrientation_python(config):
+    if not stateValidationCollision_python(config):
+        return False
+
+    #return checkAxisSimilarity
+
+"""
+def checkAxisSimilarity(orientationA, orientationB, axis, margin):
+    local xAxisA = {1, 0, 0}  -- X-axis vector (global frame) for object A
+    local yAxisA = {0, 1, 0}  -- Y-axis vector (global frame) for object A
+    local zAxisA = {0, 0, 1}  -- Z-axis vector (global frame) for object A
+
+    -- Convert orientations to rotation matrices
+    local rotMatrixA = sim.buildMatrix({0, 0, 0}, orientationA)
+    local rotMatrixB = sim.buildMatrix({0, 0, 0}, orientationB)
+
+    if axis == "x" then
+        -- Extract X-axis from the rotation matrices
+        local transformedAxisA = sim.multiplyVector(rotMatrixA, xAxisA) -- gets first column of rotation matrix - represents vector of X axis {Xx, Xy, Xz}
+        local transformedAxisB = sim.multiplyVector(rotMatrixB, xAxisA)
+    elseif axis == "y" then
+        -- Extract Y-axis from the rotation matrices
+        local transformedAxisA = sim.multiplyVector(rotMatrixA, yAxisA)
+        local transformedAxisB = sim.multiplyVector(rotMatrixB, yAxisA)
+    elseif axis == "z" then
+        -- Extract Y-axis from the rotation matrices
+        local transformedAxisA = sim.multiplyVector(rotMatrixA, zAxisA)
+        local transformedAxisB = sim.multiplyVector(rotMatrixB, zAxisA)
+    elseif axis == "free" then
+        -- just return true, as we don't need to care about fixed orientations:
+        return true
+    end
+
+    -- Check similarity between the X-axes: dot product of a vector with itself is the square of its magnitude
+    local dotProduct = transformedAxisA[1] * transformedAxisB[1] + transformedAxisA[2] * transformedAxisB[2] + transformedAxisA[3] * transformedAxisB[3]
+
+    if dotProduct > margin then -- if dotProduct is near to 1 then X axes are similarly alligned.
+        return true
+    end
+    return false
+end
+"""
 
 def findIKConfig(args):
     # -- Prepare robot collection:
@@ -367,6 +414,8 @@ def parse_args(args):
     self.robot_collection = sim.createCollection()
     sim.addItemToCollection(self.robot_collection, sim.handle_tree, self.robot, 0)
 
+    if "use_lua" in args: self.use_lua = args["use_lua"]
+
 #end
 
 
@@ -390,6 +439,8 @@ def ompl_path_planning(args):
 
     ################################################################################################
 
+    print(valid_config)
+
     final_path = None
 
     if len(valid_config) > 0:
@@ -405,19 +456,19 @@ def ompl_path_planning(args):
         simOMPL.setCollisionPairs(self.ompl_task,[self.robot_collection, sim.handle_all])
         simOMPL.setStartState(self.ompl_task,getConfig_python())
         simOMPL.setGoalState(self.ompl_task,valid_config)
+        simOMPL.setStateValidityCheckingResolution(self.ompl_task, self.state_resolution)
 
         if self.use_state_validation:
-            # WARNING: state validation is very slow, probably best not to use it:
+            # WARNING: state validation is slow, probably best not to use it:
+
             if self.motion_constraint == "free":
-                sim.addLog(sim.verbosity_default, "\t-- Considering free-axis orientation...")
+                if self.verbose: sim.addLog(sim.verbosity_default, "\t-- Considering free-axis orientation...")
+                simOMPL.setStateValidationCallback(self.ompl_task, stateValidationCollision_python)
+            elif self.motion_constraint == "fix-x":
+                if self.verbose: sim.addLog(sim.verbosity_default, "\t-- Considering fixed-axis orientation...")
+                # TODO: fix this:
+                #simOMPL.setStateValidationCallback(self.ompl_task, stateValidation)
 
-                #simOMPL.setStateValidationCallback(self.ompl_task, stateValidationCollision_python)
-                #simOMPL.setStateValidationCallback(self.ompl_task, "stateValidationCollision_lua" if self.use_lua else stateValidationCollision_python)
-            else:
-                sim.addLog(sim.verbosity_default, "\t-- Considering fixed-axis orientation...")
-                #simOMPL.setStateValidationCallback(self.ompl_task, 'luaStateValidation_orientation')
-
-        simOMPL.setStateValidityCheckingResolution(self.ompl_task, self.state_resolution)
         simOMPL.setup(self.ompl_task)
 
         for _ in range(self.num_ompl_attempts):
@@ -428,6 +479,7 @@ def ompl_path_planning(args):
                     "max_compute": self.max_compute,
                     "max_simplify": self.max_simplify,
                     "len_path": self.len_path,
+                    "use_state_validation": self.use_state_validation,
                 }
                 output = sim.callScriptFunction("luaOMPLCompute", sim.handle_self, data)
                 self.ompl_task, result, path = output[0], output[1], output[2]
@@ -450,7 +502,7 @@ def ompl_path_planning(args):
 
             if result and is_exact_solution:
                 # -- We found a collision-free path!
-                sim.addLog(sim.verbosity_scriptwarnings, f"[OMPLement] : Length of path: {int(simOMPL.getPathStateCount(self.ompl_task,path))}")
+                sim.addLog(sim.verbosity_default, f"[OMPLement] : Length of path: {int(simOMPL.getPathStateCount(self.ompl_task,path))}")
 
                 simOMPL.printTaskInfo(self.ompl_task)
 
