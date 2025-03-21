d794a421003ec62cb7c0947780e5fb3a45dec7e3
davidpaulius <paulius.david@gmail.com>
2025-03-21 16:39:40 -0400

    Updated OMPLement code, utils functions

--- OMPLement.py
+++ OMPLement.py
@@ -37,18 +37,21 @@ function stateValidationCollision_lua(config)
 end
 
 
-function luaFixedAxisData(data)
+function luaStateValidationData(data)
     -- 'robot' and 'tip' refer to the handles of the robot and the tip of the end-effector:
-    robot, tip = data["robot"], data["tip"]
+    robot = data["robot"]
+    tip = data["tip"]
 
-    -- 'axis' refers to any value in ["free", "x", "y", "z"]:
-    axis = data["axis"]
+    -- 'constrained_axis' refers to any value in ["free", "x", "y", "z"]:
+    axis = data["constrained_axis"]
 
-    -- this value will reflect how "aligned" the poses must be along a given axis:
-    if not data["alignment"] then
-        margin = 0.8
-    else
-        margin = data["alignment"]
+    -- 'sim_threshold' will reflect how "aligned" the poses must be along a given axis;
+    --     for *dot product similarity* -- if two vectors are aligned, their dot product will be equal to 1:
+    sim_threshold = data["alignment"]
+
+    pose_limits = nil
+    if data["pose_limits"] then
+        pose_limits = data["pose_limits"]
     end
 
     -- TODO: are there any other values needed?
@@ -56,28 +59,73 @@ end
 
 
 function stateValidationFixedAxis_lua(config)
-    -- check if a configuration is valid, i.e., doesn't collide:
     -- save current config:
     local tmp = getConfig_lua()
 
-    initial_pose = sim.getObjectPose(tip, -1)
+    -- get the pre-motion pose of the end-effector's tip:
+    local initial_pose = sim.getObjectPose(tip, -1)
 
     -- apply new config:
     setConfig_lua(config)
 
-    next_pose = sim.getObjectPose(tip, -1)
+    -- get the new pose of the end-effector's tip after moving to the candidate config:
+    local next_pose = sim.getObjectPose(tip, -1)
+
+    -- does new config collide?
+    local is_collision, _ = sim.checkCollision(robot_collection, sim.handle_all)
 
     -- restore original config:
     setConfig_lua(tmp)
 
-    -- NOTE: the initial pose of the robot's hand will be passed by auxiliary function:
-    --      variable names: axis, margin, initial_pose
-    if checkAxisSimilarity_lua(initial_pose, next_pose, axis, margin) == false then
+    if (is_collision ~= 0) then
         return false
     end
 
-    -- if the axis alignment test passes, then we check for collisions:
-    return stateValidationCollision_lua(config)
+    -- NOTE: the initial pose of the robot's hand will be passed by auxiliary function:
+    --  variable names: axis, margin, initial_pose
+    local is_aligned = checkAxisSimilarity_lua(initial_pose, next_pose, axis, margin)
+
+    -- if we have passed both checks, then this is a valid state:
+    return (is_aligned == true)
+end
+
+
+function stateValidationPoseLimits_lua(config)
+    -- check if a configuration is valid, i.e., doesn't collide:
+    if not stateValidationCollision_lua(config) then
+        return false
+    end
+
+    -- save current config:
+    local tmp = getConfig_lua()
+
+    -- apply new config:
+    setConfig_lua(config)
+
+    -- get the new pose of the end-effector's tip after moving to the candidate config:
+    local next_pose = sim.getObjectPose(tip, -1)
+
+    -- does new config collide?
+    local is_collision, _ = sim.checkCollision(robot_collection, sim.handle_all)
+
+    -- restore original config:
+    setConfig_lua(tmp)
+
+    if (is_collision ~= 0) then
+        return false
+    end
+
+    -- we will focus on the orientation portion (quaternions) of the pose:
+    for X=4,#next_pose,1 do
+        if next_pose[X] > pose_limits['upper'][X] then
+            return false
+        elif next_pose[X] < pose_limits['lower'][X] then
+            return false
+        end
+    end
+
+    -- if we have passed both checks, then this is a valid state:
+    return true
 end
 
 
@@ -92,7 +140,7 @@ function luaFindIKConfig(data)
 
     params = {
         maxDist = 0.05,
-        maxTime = 3,
+        maxTime = 1,
         findMultiple = false, -- change to True to find multiple solutions
         pMetric = {0.05,0.05,0.05,0.1},
         cb = stateValidationCollision_lua
@@ -124,31 +172,13 @@ function luaOMPLCompute(data)
 end
 
 
--- Function to check if the X-axis of object B is similar to the X-axis of object A
-function checkXAxisSimilarity(orientationA, orientationB, margin)
-    local xAxisA = {1, 0, 0}  -- X-axis vector (global frame) for object A
-
-    -- Convert orientations to rotation matrices
-    local rotMatrixA = sim.buildMatrix({0, 0, 0}, orientationA)
-    local rotMatrixB = sim.buildMatrix({0, 0, 0}, orientationB)
-
-    -- Extract X-axis from the rotation matrices
-    local transformedXAxisA = sim.multiplyVector(rotMatrixA, xAxisA) -- gets first column of rotation matrix - represents vector of X axis {Xx, Xy, Xz}
-    local transformedXAxisB = sim.multiplyVector(rotMatrixB, xAxisA)
-
-    -- Check similarity between the X-axes: dot product of a vector with itself is the square of its magnitude
-    local dotProduct = transformedXAxisA[1] * transformedXAxisB[1] + transformedXAxisA[2] * transformedXAxisB[2] + transformedXAxisA[3] * transformedXAxisB[3]
-
-    if dotProduct > margin then -- if dotProduct is near to 1 then X axes are similarly aligned.
-        return true
-    end
-    return false
-end
-
-
-function checkAxisSimilarity_lua(poseA, poseB, axis, margin)
+---------------------------------------------------------------------------------------
+-- NOTE: this function is a modified version of a function from the following thread:
+--      https://forum.coppeliarobotics.com/viewtopic.php?t=10200
+---------------------------------------------------------------------------------------
+function checkAxisSimilarity_lua(poseA, poseB, axis, sim_threshold)
     -- NOTE: axis can be some kind of string in ["x", "y", "z", "free"],
-    --  'margin' refers to how "similar" the axis must be aligned to some given reference:
+    --  'sim_threshold' refers to how "similar" the axis must be aligned to some given reference:
     local xAxisA = {1, 0, 0}  -- X-axis vector (global frame) for object A
     local yAxisA = {0, 1, 0}  -- Y-axis vector (global frame) for object A
     local zAxisA = {0, 0, 1}  -- Z-axis vector (global frame) for object A
@@ -186,7 +216,7 @@ function checkAxisSimilarity_lua(poseA, poseB, axis, margin)
         + (transformedAxisA[2] * transformedAxisB[2])
         + (transformedAxisA[3] * transformedAxisB[3])
 
-    if dotProduct > margin then -- if dotProduct is near to 1 then X axes are similarly alligned.
+    if dotProduct > sim_threshold then -- if dotProduct is near to 1 then X axes are similarly alligned.
         return true
     end
 
@@ -398,6 +428,10 @@ def parse_args(args):
     assert "robot" in args, "[OMPLement] : Robot name not defined!"
     self.robot_name = args["robot"]
 
+    # -- make the robot collection a global object:
+    self.robot_collection = sim.createCollection()
+    sim.addItemToCollection(self.robot_collection, sim.handle_tree, self.robot, 0)
+
     # NOTE: we will create a dummy object representing the target for planning!
     # -- extract the goal object given as input to this function:
     assert "goal" in args, "[OMPLement] : Goal not defined!"
@@ -432,22 +466,27 @@ def parse_args(args):
         self.ompl_state_resolution = args["ompl_state_resolution"]
     else:
         # -- by default, use this resolution value:
-        self.ompl_state_resolution = float("1.0e-2")
+        self.ompl_state_resolution = float("5.0e-3")
 
-    # -- ompl_motion_constraint :- this will indicate whether some axis needs to be fixed for path planning:
+    # NOTE: we should always use lua whenever possible since it is faster:
+    if "ompl_use_lua" in args:
+        self.ompl_use_lua = args["ompl_use_lua"]
+
+    # -- ompl_use_state_validation :- this indicates whether state validation will be used in OMPL computation:
+    self.ompl_use_state_validation = True
+    if "ompl_use_state_validation" in args:
+        self.ompl_use_state_validation = args["ompl_use_state_validation"]
+
+    # -- ompl_motion_constraint :- this indicates whether some axis needs to be fixed for path planning:
     self.ompl_motion_constraint = "free"
     if "ompl_motion_constraint" in args:
         self.ompl_motion_constraint = args["ompl_motion_constraint"]
 
-    # -- make the robot collection a global object:
-    self.robot_collection = sim.createCollection()
-    sim.addItemToCollection(self.robot_collection, sim.handle_tree, self.robot, 0)
-
-    if "ompl_use_lua" in args:
-        self.ompl_use_lua = args["ompl_use_lua"]
-
-    if "ompl_use_state_validation" in args:
-        self.ompl_use_state_validation = args["ompl_use_state_validation"]
+    # NOTE: rather than restricting motion based on a single axis as above,
+    #   we can indicate limits for each axis:
+    self.ompl_pose_limits = None
+    if "ompl_pose_limits" in args:
+        self.ompl_pose_limits = args["ompl_pose_limits"]
 
 #end
 
@@ -526,8 +565,7 @@ def ompl_path_planning(args):
         simOMPL.setVerboseLevel(self.ompl_task, 1)
 
         if self.ompl_use_state_validation:
-            # WARNING: state validation is slow, probably best not to use it:
-
+            # NOTE: state validation is typically slow, but pretty decent when used in lua mode:
             if self.ompl_motion_constraint == "free":
                 if self.verbose:
                     sim.addLog(sim.verbosity_default, "[OMPLement] : Considering free-axis orientation...")
@@ -536,22 +574,28 @@ def ompl_path_planning(args):
                 else:
                     simOMPL.setStateValidationCallback(self.ompl_task, 'stateValidationCollision_lua')
 
-            elif self.ompl_motion_constraint in ["x", "y", "z"]:
-                if self.verbose:
-                    sim.addLog(sim.verbosity_default, f"[OMPLement] : Considering fixed-axis orientation (axis={self.ompl_motion_constraint})...")
-
-                # TODO: fix this:
-                data = {
+            else:
+                # -- we are adding extra constraints for state validation (beyond collision):
+                state_validation_data = {
                     "robot": self.robot,
                     "tip": self.tip,
-                    "axis": self.ompl_motion_constraint,
-                    "alignment": 0.990,
+                    "alignment": 0.99,
+                    "constrained_axis": self.ompl_motion_constraint,
+                    "pose_limits": self.ompl_pose_limits,
                 }
+
                 # -- send some necessary values to the state validation checker:
-                sim.callScriptFunction("luaFixedAxisData", sim.handle_self, data)
-                if not self.ompl_use_lua:
-                    simOMPL.setStateValidationCallback(self.ompl_task, "stateValidationFixedAxis_lua")
-                else:
+                sim.callScriptFunction("luaStateValidationData", sim.handle_self, state_validation_data)
+
+                if self.ompl_pose_limits:
+                    # -- we are going to use the lua version no matter what:
+                    simOMPL.setStateValidationCallback(self.ompl_task, "stateValidationPoseLimits_lua")
+
+                elif self.ompl_motion_constraint in ["x", "y", "z"]:
+                    if self.verbose:
+                        sim.addLog(sim.verbosity_default, f"[OMPLement] : Considering fixed-axis orientation (axis={self.ompl_motion_constraint})...")
+
+                    # -- we are going to use the lua version no matter what:
                     simOMPL.setStateValidationCallback(self.ompl_task, "stateValidationFixedAxis_lua")
 
         simOMPL.setup(self.ompl_task)
--- function_call.py
+++ function_call.py
@@ -1,13 +1,9 @@
 import os
 import sys
-import time
-import math
 from random import randint, shuffle
+from typing import Type
 
-from driver import Interfacer
-
-from scipy.interpolate import CubicSpline
-import numpy as np
+from utils import Interfacer
 
 try:
     from coppeliasim_zmqremoteapi_client import RemoteAPIClient
@@ -19,189 +15,12 @@ except ImportError:
 robot_name = "Panda"
 
 sim_interfacer = Interfacer(
-    scene_file_name='./panda_blocks_simple.ttt',
+    # scene_file_name='./panda_cocktail.ttt',
+    # scene_file_name="C:/Users/david/OLP_LLM/utamp/scenes/panda_blocks_randomized_2025-02-27_13-14-46S.ttt",
+    scene_file_name="C:/Users/david/OLP_LLM/utamp/scenes/panda_blocks_randomized_2025-03-21_15-40-42S.ttt",
     robot_name=robot_name,
     robot_gripper=f"{robot_name}_gripper",
 )
-
-def pick(
-    target_object: str,
-    ompl_args: dict = {},
-    affordance: str = 'pick-top',
-):
-    start_time = time.time()
-
-    goal_achieved = False
-
-    # -- we will try to find configs with OMPL and find a collision-avoiding plan up to a certain number of times
-    for _ in range(3 if target_object in ['table', 'worksurface'] else 1):
-        # -- we will be generous and allow three attempts at finding an empty place on the table:
-        goal_poses = sim_interfacer.ompl_find_pose(target_object, affordance)
-
-        for G in range(len(goal_poses)):
-            # -- use the OMPL-based path planning method:
-            success = sim_interfacer.ompl_path_planning(
-                target_object,
-                goal_poses[G],
-                dict(ompl_args),
-            )
-
-            # -- check if we succeeded with this particular pose:
-            if success:
-                goal_achieved = goal_poses[G]
-                break
-
-        if bool(goal_achieved): break
-
-    # -- if we could not find a plan, then we just return:
-    if not goal_achieved: return False
-
-    # -- close the gripper
-    sim_interfacer.act_end_effector(action=1)
-
-    time.sleep(0.5)
-
-    # -- move the end-effector up:
-    goal_achieved[2] += 0.1
-
-    # NOTE: post-grasping check below:
-    # -- get the object handles for the gripper's attach point:
-    gripper_attachPoint = -1
-    children = sim_interfacer.sim.getObjectsInTree(sim_interfacer.sim.getObject(f"/{sim_interfacer.robot_name}"))
-    for C in children:
-        if "attachPoint" in sim_interfacer.sim.getObjectAlias(C):
-            gripper_attachPoint = C
-
-    obj_in_hand = sim_interfacer.sim.getObjectChild(gripper_attachPoint, 0)
-    if obj_in_hand > -1:
-        # -- this means that we want to move the gripper up to remove the object from the top of the below object's surface:
-        start = sim_interfacer.sim.getObjectPosition(sim_interfacer.sim.getObject(f'/{sim_interfacer.robot_name}/target'), sim_interfacer.sim.getObject(f'/{sim_interfacer.robot_name}')) + sim_interfacer.sim.getObjectOrientation(sim_interfacer.sim.getObject(f'/{sim_interfacer.robot_name}/target'), sim_interfacer.sim.getObject(f'/{sim_interfacer.robot_name}'))
-        end = list(start)
-        # -- we want to move up by half the height of the object:
-        # end[2] += sim_interfacer.sim.getObjectFloatParam(obj_in_hand, sim_interfacer.sim.objfloatparam_objbbox_max_z)
-        end[2] += 0.02
-
-        # -- we also want to pick up the object and align it with the base of the robot:
-
-        traj_move_up = sim_interfacer.spline_adjust_trajectory(
-            sim_interfacer.generate_trajectory({'time': [0, 1], 'trajectory': [start, end]}, ntraj=25)
-        )
-
-        # -- execute the trajectory but do not change the state of the gripper:
-        sim_interfacer.execute_trajectory(traj_move_up)
-
-        if target_object in sim_interfacer.sim.getObjectAlias(obj_in_hand):
-        # -- this means that we have successfully grasped the intended object:
-            return True
-
-    print(f'\t-- total time: {time.time() - start_time}')
-
-    return False
-
-
-def place(
-    target_object: str,
-    ompl_args: dict = {},
-    affordance: str = 'place-top',
-):
-    start_time = time.time()
-
-    goal_achieved = False
-
-    # -- we will try to find configs with OMPL and find a collision-avoiding plan up to a certain number of times
-    for _ in range(3 if target_object in ['table', 'worksurface'] else 1):
-        # -- we will be generous and allow three attempts at finding an empty place on the table:
-        goal_poses = sim_interfacer.ompl_find_pose(target_object, affordance)
-
-        for goal in goal_poses:
-            # -- use the OMPL-based path planning method:
-            success = sim_interfacer.ompl_path_planning(
-                target_object,
-                goal,
-                ompl_args,
-            )
-
-            # -- check if we succeeded with this particular pose:
-            if success:
-                goal_achieved = success
-                break
-
-        if goal_achieved: break
-
-    # -- if we could not find a plan, then we just return:
-    if not goal_achieved: return False
-
-    # -- close the gripper
-    sim_interfacer.act_end_effector(action=0)
-
-    time.sleep(1)
-
-    print(f'\t-- total time: {time.time() - start_time}')
-
-    return True
-
-
-def pour(
-    source_container: str,
-    target_container: str,
-    ompl_args: dict = {},
-):
-    start_time = time.time()
-
-    # NOTE: pouring comprises of the following subactions:
-    #   1. pre-pouring: positioning a source container to a target container
-    #   3. rotate the source container to transfer contents
-    #   4. place the source container back somewhere
-
-    # -- now, we need to find a path for pre-pouring:
-    while True:
-        result = sim_interfacer.ompl_path_planning(
-            target_object=target_container,
-            goal_pose=sim_interfacer.ompl_find_pose(
-                target_object=target_container,
-                affordance='pour',
-            )[0],
-            ompl_args=ompl_args,
-        )
-
-        if result: break
-
-    target = sim_interfacer.sim.getObject(f'/{sim_interfacer.robot_name}/target')
-    robot = sim_interfacer.sim.getObject(f'/{sim_interfacer.robot_name}')
-
-    src_object_pos = sim_interfacer.sim.getObjectPosition(target, robot)
-    tgt_object_pos = sim_interfacer.sim.getObjectPosition(sim_interfacer.sim.getObject(f"/{target_container}"), robot)
-
-    # -- let's determine the direction of pouring:
-    pour_direction = 'left' if src_object_pos[1] > tgt_object_pos[1] else 'right'
-
-    # -- this is the maximum angle we will perform rotation:
-    max_rotation = 120
-
-    # -- get the initial pose of the gripper:
-    start = sim_interfacer.sim.getObjectPose(target, robot)
-
-    trajectory = [start]
-
-    # -- rotate forward...
-    for angle in range(max_rotation):
-        end = sim_interfacer.sim.rotateAroundAxis(start, [1, 0, 0], start[:3], (angle * math.pi/180) * (-1.0 if pour_direction == "right" else 1.0))
-        sim_interfacer.sim.setObjectPose(target, end, robot)
-        trajectory.append(list(end))
-        time.sleep(0.001)
-
-    time.sleep(1)
-    trajectory.reverse()
-
-    # ... and back!
-    for pose in trajectory:
-        sim_interfacer.sim.setObjectPose(target, pose, robot)
-        time.sleep(0.001)
-
-    print(f'\t-- total time: {time.time() - start_time}')
-
-    return True
-
 
 all_blocks = ["B_block_1", "D_block_1", "C_block_1", "P_block_1"]
 shuffle(all_blocks)
@@ -209,26 +28,12 @@ shuffle(all_blocks)
 setting = randint(0,1)
 setting = 0
 
-sim_interfacer.start()
+sim_interfacer.sim_start()
 
-if bool(setting):
-    for obj in all_blocks:
-        success = sim_interfacer.execute(
-            target_object=obj,
-            ompl_args={
-                "ompl_state_resolution": float("5.0e-3"),
-                "ompl_use_lua": True,
-                "ompl_algorithm": "RRTConnect",
-                "ompl_motion_constraint": "y",
-            },
-            gripper_action=0
-        )
-else:
-    print("stacking")
-
+if setting == 2:
     # -- do series of block-stacking:
     for x in range(1, len(all_blocks)):
-        success = pick(
+        success = sim_interfacer.pick(
             target_object=all_blocks[x],
             ompl_args={
                 "ompl_state_resolution": float("2.5e-3"),
@@ -239,7 +44,7 @@ else:
                 "ompl_max_simplify": 15,
             })
 
-        success = place(
+        success = sim_interfacer.place(
             target_object=all_blocks[x-1],
             ompl_args={
                 "ompl_state_resolution": float("2.5e-3"),
@@ -250,89 +55,151 @@ else:
                 "ompl_max_simplify": 15,
             },
             affordance='place-top')
-
-    success = pick(
+elif setting == 2:
+    success = sim_interfacer.pick(
         target_object="bottle_vodka",
         ompl_args={
             "ompl_state_resolution": float("2.5e-3"),
             "ompl_use_lua": True,
             "ompl_algorithm": "RRTConnect",
             "ompl_motion_constraint": "free",
-            "ompl_max_compute": 15,
-            "ompl_max_simplify": 15,
+            "ompl_max_compute": 5,
         },
         affordance='pick-side',
     )
 
-    if not success: sys.exit("ERROR: no object in hand for pouring!")
+    if not success:
+        print("ERROR: no object in hand for pouring!")
 
-    success = pour(
+    success = sim_interfacer.pour(
         source_container="bottle_vodka",
-        target_container="drinking_glass",
+        target_container="Cup",
         ompl_args={
             "ompl_state_resolution": float("2.5e-3"),
             "ompl_use_lua": True,
             "ompl_algorithm": "RRTConnect",
-            "ompl_motion_constraint": "x",
+            "ompl_motion_constraint": "z",
             "ompl_max_compute": 15,
-            "ompl_max_simplify": 15,
         },
     )
 
-    success = place(
+    success = sim_interfacer.place(
         target_object="table",
         ompl_args={
             "ompl_state_resolution": float("2.5e-3"),
             "ompl_use_lua": True,
             "ompl_algorithm": "RRTConnect",
-            "ompl_motion_constraint": "x",
+            "ompl_motion_constraint": "z",
             "ompl_max_compute": 15,
-            "ompl_max_simplify": 15,
-        },
-        affordance='place-top',
-    )
-
-    success = pick(
-        target_object="drinking_glass",
-        ompl_args={
-            "ompl_state_resolution": float("2.5e-3"),
-            "ompl_use_lua": True,
-            "ompl_algorithm": "RRTConnect",
-            "ompl_motion_constraint": "free",
-            "ompl_max_compute": 15,
-            "ompl_max_simplify": 15,
-        },
-        affordance='pick-side',
-    )
-
-    if not success: sys.exit("ERROR: no object in hand for pouring!")
-
-
-    success = pour(
-        source_container="drinking_glass",
-        target_container="drinking_glass_2",
-        ompl_args={
-            "ompl_state_resolution": float("2.5e-3"),
-            "ompl_use_lua": True,
-            "ompl_algorithm": "RRTConnect",
-            "ompl_motion_constraint": "x",
-            "ompl_max_compute": 15,
-            "ompl_max_simplify": 15,
         },
     )
 
-    success = place(
-        target_object="table",
-        ompl_args={
-            "ompl_state_resolution": float("2.5e-3"),
-            "ompl_use_lua": True,
-            "ompl_algorithm": "RRTConnect",
-            "ompl_motion_constraint": "x",
-            "ompl_max_compute": 15,
-            "ompl_max_simplify": 15,
-        },
-        affordance='place-top',
-    )
+    # success = place(
+    #     target_object="table",
+    #     ompl_args={
+    #         "ompl_state_resolution": float("2.5e-3"),
+    #         "ompl_use_lua": True,
+    #         "ompl_algorithm": "RRTConnect",
+    #         "ompl_motion_constraint": "x",
+    #         "ompl_max_compute": 15,
+    #         "ompl_max_simplify": 15,
+    #     },
+    #     affordance='place-top',
+    # )
 
-sim_interfacer.stop()
+    # success = pick(
+    #     target_object="drinking_glass",
+    #     ompl_args={
+    #         "ompl_state_resolution": float("2.5e-3"),
+    #         "ompl_use_lua": True,
+    #         "ompl_algorithm": "RRTConnect",
+    #         "ompl_motion_constraint": "free",
+    #         "ompl_max_compute": 15,
+    #         "ompl_max_simplify": 15,
+    #     },
+    #     affordance='pick-side',
+    # )
+
+    # if not success: sys.exit("ERROR: no object in hand for pouring!")
+
+
+    # success = pour(
+    #     source_container="drinking_glass",
+    #     target_container="drinking_glass_2",
+    #     ompl_args={
+    #         "ompl_state_resolution": float("2.5e-3"),
+    #         "ompl_use_lua": True,
+    #         "ompl_algorithm": "RRTConnect",
+    #         "ompl_motion_constraint": "x",
+    #         "ompl_max_compute": 15,
+    #         "ompl_max_simplify": 15,
+    #     },
+    # )
+
+    # success = place(
+    #     target_object="table",
+    #     ompl_args={
+    #         "ompl_state_resolution": float("2.5e-3"),
+    #         "ompl_use_lua": True,
+    #         "ompl_algorithm": "RRTConnect",
+    #         "ompl_motion_constraint": "x",
+    #         "ompl_max_compute": 15,
+    #         "ompl_max_simplify": 15,
+    #     },
+    #     affordance='place-top',
+    # )
+
+# success = sim_interfacer.pick(
+#     target_object="bottle_vodka",
+#     ompl_args={
+#         "ompl_state_resolution": float("2.5e-3"),
+#         "ompl_use_lua": True,
+#         "ompl_algorithm": "RRTConnect",
+#         "ompl_motion_constraint": "free",
+#         "ompl_max_compute": 15,
+#     })
+
+success = sim_interfacer.pick(
+    target_object="P_block_2",
+    ompl_args={
+        "ompl_state_resolution": float("2.5e-3"),
+        "ompl_use_lua": True,
+        "ompl_algorithm": "RRTConnect",
+        "ompl_motion_constraint": "free",
+        "ompl_max_compute": 5,
+    })
+
+success = sim_interfacer.place(
+    target_object="T_block_1",
+    ompl_args={
+        "ompl_state_resolution": float("2.5e-3"),
+        "ompl_use_lua": True,
+        "ompl_algorithm": "RRTConnect",
+        "ompl_motion_constraint": "z",
+        "ompl_max_compute": 15,
+    })
+
+# success = sim_interfacer.pick(
+#     target_object="S_block_1",
+#     ompl_args={
+#         "ompl_state_resolution": float("2.5e-3"),
+#         "ompl_use_lua": True,
+#         "ompl_algorithm": "RRTConnect",
+#         "ompl_motion_constraint": "free",
+#         "ompl_max_compute": 5,
+#     })
+
+# success = sim_interfacer.place(
+#     target_object="E_block_2",
+#     ompl_args={
+#         "ompl_state_resolution": float("2.5e-3"),
+#         "ompl_use_lua": True,
+#         "ompl_algorithm": "RRTConnect",
+#         "ompl_motion_constraint": "z",
+#         "ompl_max_compute": 15,
+    # })
+
+sim_interfacer.return_home()
+
+sim_interfacer.sim_stop()
 pass
